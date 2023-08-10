63e2ba3a216127056f788140221b902930ae6760
Uyouyou <usiwosonia@gmail.com>
2023-08-09 19:04:02 -0500

    Initial Commit

---
+++ orders.js
@@ -0,0 +1,36 @@
+const orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'pending',
+  },
+
+  {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Due',
+    shipping: 'Declined',
+  },
+
+  {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'pending',
+  },
+
+  {
+    productName: 'Drone with caera Drone',
+    productNumber: '96996',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+  },
+
+  {
+    productName: 'GPS 4k Drone',
+    productNumber: '22921',
+    paymentStatus: 'paid',
+    shipping: 'Delivered',
+  },
+];
